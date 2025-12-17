d456b52ca318a4d42d689a89faa996513c71c3f1
capitolgrape <karulkem@gmail.com>
2025-12-17 14:39:43 +0300

    update

--- install.sh
+++ install.sh
@@ -6,7 +6,13 @@ exec > >(tee -i install.log) 2>&1
 
 paru -S --needed --noconfirm - < pkgs
 
+if command -v fish &> /dev/null; then
+    chsh -s $(which fish)
+fi
+
 curl -sS https://starship.rs/install.sh | sh
+
+starship preset pure-preset -o ~/.config/starship.toml
 
 ./scripts/code_ext.sh
 
@@ -20,12 +26,7 @@ for dir in niri waybar kitty fish hypr; do
     fi
 done
 
-starship preset pure-preset -o ~/.config/starship.toml
-
-if command -v fish &> /dev/null; then
-    chsh -s $(which fish)
-fi
+fc-cache -fv
 
 sudo systemctl enable --now bluetooth.service
-
-fc-cache -fv
+sudo systemctl enable --now ly.service
