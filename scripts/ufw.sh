411c576605e76130929b60c25a2ae8e8bc9bbd55
kemal <karul@tuta.io>
2026-03-10 17:10:54 +0300

    add ufw setup script

---
+++ scripts/ufw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if ! command -v ufw >/dev/null 2>&1; then
+    echo "ufw is not installed. Exiting."
+    exit 1
+fi
+
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw --force enable
+
+echo "ufw setup complete."
