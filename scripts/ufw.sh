a154f5e08aac326b566908dbefe8b6dc95d80b23
kemal <karul@tuta.io>
2026-03-10 19:51:32 +0300

    ufw for desktop

--- scripts/ufw.sh
+++ scripts/ufw.sh
@@ -8,8 +8,6 @@ if ! command -v ufw >/dev/null 2>&1; then
 fi
 
 sudo ufw limit 22/tcp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw --force enable
