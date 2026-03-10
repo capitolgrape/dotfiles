ca11f8d5ceab988b56047812936449d62d59e125
kemal <karul@tuta.io>
2026-03-10 18:19:53 +0300

    improve paru installer cleanup

--- scripts/aur_helper.sh
+++ scripts/aur_helper.sh
@@ -11,13 +11,20 @@ sudo pacman -S --needed --noconfirm base-devel git
 BUILD_DIR=$(mktemp -d)
 original_dir=$(pwd)
 
+cleanup() {
+    cd "$original_dir" || exit 1
+    rm -rf "$BUILD_DIR"
+}
+
+trap cleanup EXIT
+
 cd "$BUILD_DIR" || exit 1
 
 git clone https://aur.archlinux.org/paru.git
 cd paru || exit 1
 makepkg -si --noconfirm
 
-cd "$original_dir"
-rm -rf "$BUILD_DIR"
+trap - EXIT
+cleanup
 
 echo "paru installation completed successfully."
