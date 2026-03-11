34b2e325c653ef0f12c7e8b9582a669b385cadc8
kemal <karul@tuta.io>
2026-03-11 17:23:38 +0300

    im dumb

--- scripts/auto_cpufreq.sh
+++ scripts/auto_cpufreq.sh
@@ -4,23 +4,13 @@
 
 set -eu
 
-AUTO_CPUFREQ_PATH="$HOME/.local/share/auto-cpufreq"
-
 if ! command -v auto-cpufreq >/dev/null 2>&1; then
-    if command -v powerprofilesctl >/dev/null 2>&1; then
-        sudo systemctl disable --now power-profiles-daemon
-    fi
+    echo "auto-cpufreq is not installed, skipping."
+    exit 0
+fi
 
-    mkdir -p "$HOME/.local/share"
-
-    if [ -d "$AUTO_CPUFREQ_PATH" ]; then
-        rm -rf "$AUTO_CPUFREQ_PATH"
-    fi
-
-    git clone --depth=1 https://github.com/AdnanHodzic/auto-cpufreq.git "$AUTO_CPUFREQ_PATH"
-    cd "$AUTO_CPUFREQ_PATH"
-    sudo ./auto-cpufreq-installer
-    sudo auto-cpufreq --install
+if command -v powerprofilesctl >/dev/null 2>&1; then
+    sudo systemctl disable --now power-profiles-daemon
 fi
 
 if ls /sys/class/power_supply/BAT* >/dev/null 2>&1; then
