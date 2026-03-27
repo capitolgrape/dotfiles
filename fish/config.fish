fce3a0ff243dcb5311e64352cf3c1768450cae1a
kemal <karul@tuta.io>
2026-03-27 19:08:19 +0300

    minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,7 @@ if status is-interactive
     abbr -a gc "git commit"
     abbr -a gp "git push"
     abbr -a ff "clear && fastfetch"
+    abbr -a udb 'sudo rm -v /var/lib/pacman/db.lck'
 
     if command -q lsd
         alias ls="lsd --human-readable --literal --group-directories-first --color=auto"
@@ -20,5 +21,4 @@ if status is-interactive
         alias ls="ls --human-readable --literal --group-directories-first --color=auto"
     end
 
-    alias udb="sudo rm -rf /var/lib/pacman/db.lck"
 end
